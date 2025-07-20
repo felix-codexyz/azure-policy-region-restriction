6d63fae23c4959c3e7013f5f953f556a0c8adc17
Felix M <felixmomodebe@gmail.com>
2025-07-20 19:18:07 -0400

    fixed relative path errors

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,9 @@ variable "subscription_id" {
   type        = string
   description = "The Azure subscription ID where the policy will be assigned"
 }
+
+variable "location" {
+  description = "Azure region for resources"
+  type        = string
+  default     = "eastus"
+}
